8efe88a456208493ebcf0203e5504752e728b80a
ayo <a2yomidiscord@gmail.com>
2025-08-15 16:01:16 +0300

    [sc-19] chore: setup pnpm workspaces

--- src/utils/enums/default-colors.ts
+++
@@ -1,8 +0,0 @@
-import type { LiteralEnum } from "@ts-fetcher/types";
-
-export const DefaultColors = {
-  Default: 0x2c2f33,
-  Error: 0xf44336,
-} as const;
-
-export type DefaultColors = LiteralEnum<typeof DefaultColors>;
