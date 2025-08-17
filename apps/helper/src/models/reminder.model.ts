15e10575004500c20b334880ba7fd1dfb95553cb
ayo <a2yomidiscord@gmail.com>
2025-08-17 21:04:41 +0300

    tests: added tests for parser

--- apps/helper/src/models/reminder.model.ts
+++ apps/helper/src/models/reminder.model.ts
@@ -1,14 +1,5 @@
-import type { LiteralEnum } from "@fear/utils";
 import { getModelForClass, prop } from "@typegoose/typegoose";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses.js";
-
-export const RemindType = {
-  DiscordMonitoring: 0,
-  ServerMonitoring: 1,
-  SdcMonitoring: 2,
-} as const;
-
-export type RemindType = LiteralEnum<typeof RemindType>;
 
 export class Remind extends TimeStamps {
   @prop({ alias: "guild_id", required: true })
