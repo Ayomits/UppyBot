c81c24f7b4d53abbbd36a78d758009afbe1481d3
ayo <a2yomidiscord@gmail.com>
2025-08-17 21:04:41 +0300

    tests: added tests for parser

---
+++ apps/helper/src/controllers/reminder/reminder.parser.spec.ts
@@ -0,0 +1,193 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "reflect-metadata";
+
+import type { Message } from "discord.js";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+
+import {
+  MonitoringBot,
+  MonitoringBotMessage,
+  RemindType,
+} from "./reminder.const";
+import { ReminderParser } from "./reminder.parser";
+
+const now = new Date();
+const guildId = "123";
+const userId = "1234";
+
+function createMockMessage(partial: Partial<Message>): Message {
+  return {
+    author: { id: userId } as any,
+    guildId: guildId,
+    embeds: [],
+    interactionMetadata: { user: { id: userId } } as any,
+    ...partial,
+  } as Message;
+}
+
+function createMockEmbed(description: string, timestamp?: Date) {
+  return {
+    description,
+    timestamp: timestamp?.getTime(),
+  };
+}
+
+describe("ReminderParser", () => {
+  let parser: ReminderParser;
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(now);
+
+    parser = new ReminderParser();
+  });
+
+  beforeEach(() => {
+    parser = new ReminderParser();
+  });
+
+  test("getHandler", () => {
+    expect(parser.getHandler(MonitoringBot.DiscordMonitoring)).toBe(
+      parser.handleDiscordMonitoring,
+    );
+
+    expect(parser.getHandler(MonitoringBot.ServerMonitoring)).toBe(
+      parser.handleServerMonitoring,
+    );
+
+    expect(parser.getHandler(MonitoringBot.SdcMonitoring)).toBe(
+      parser.handleSdcMonitoring,
+    );
+  });
+
+  test("handleDiscordMonitoring (success russian)", () => {
+    expect(
+      parser.handleDiscordMonitoring(
+        createMockMessage({
+          guildId: guildId,
+          embeds: [
+            // @ts-expect-error its test
+            createMockEmbed(
+              MonitoringBotMessage.discordMonitoring.success[0],
+              now,
+            ),
+          ],
+        }) as any,
+      ),
+    ).toStrictEqual(
+      parser.handleSuccess(now, guildId, userId, RemindType.DiscordMonitoring),
+    );
+  });
+
+  test("handleDiscordMonitoring (success english)", () => {
+    expect(
+      parser.handleDiscordMonitoring(
+        createMockMessage({
+          guildId: guildId,
+          embeds: [
+            // @ts-expect-error its test
+            createMockEmbed(
+              MonitoringBotMessage.discordMonitoring.success[1],
+              now,
+            ),
+          ],
+        }) as any,
+      ),
+    ).toStrictEqual(
+      parser.handleSuccess(now, guildId, userId, RemindType.DiscordMonitoring),
+    );
+  });
+
+  test("handleDiscordMonitoring (failure english)", () => {
+    expect(
+      parser.handleDiscordMonitoring(
+        createMockMessage({
+          guildId: guildId,
+          embeds: [
+            // @ts-expect-error its test
+            createMockEmbed(
+              MonitoringBotMessage.discordMonitoring.failure[1],
+              now,
+            ),
+          ],
+        }) as any,
+      ),
+    ).toStrictEqual(
+      parser.handleFailure(now, guildId, userId, RemindType.DiscordMonitoring),
+    );
+  });
+
+  test("handleDiscordMonitoring (failure russian)", () => {
+    expect(
+      parser.handleDiscordMonitoring(
+        createMockMessage({
+          guildId: guildId,
+          embeds: [
+            // @ts-expect-error its test
+            createMockEmbed(
+              MonitoringBotMessage.discordMonitoring.failure[1],
+              now,
+            ),
+          ],
+        }) as any,
+      ),
+    ).toStrictEqual(
+      parser.handleFailure(now, guildId, userId, RemindType.DiscordMonitoring),
+    );
+  });
+
+  test("handleDiscordMonitoring (failure another language)", () => {
+    expect(
+      parser.handleDiscordMonitoring(
+        createMockMessage({
+          guildId: guildId,
+          embeds: [
+            // @ts-expect-error its test
+            createMockEmbed("92482340", now),
+          ],
+        }) as any,
+      ),
+    ).toStrictEqual(
+      parser.handleFailure(now, guildId, userId, RemindType.DiscordMonitoring),
+    );
+  });
+
+  test("handleSdcMonitoring (success)", () => {
+    const timestamp = now.getTime();
+    expect(
+      parser.handleSdcMonitoring(
+        createMockMessage({
+          embeds: [
+            // @ts-expect-error its tests
+            createMockEmbed(
+              MonitoringBotMessage.sdcMonitoring.success +
+                " " +
+                `<t:${timestamp}:R>`,
+            ),
+          ],
+        }),
+      ),
+    ).toStrictEqual(
+      parser.handleSuccess(now, guildId, userId, RemindType.SdcMonitoring),
+    );
+  });
+
+  test("handleSdcMonitoring (failure)", () => {
+    expect(
+      parser.handleSdcMonitoring(
+        createMockMessage({
+          embeds: [
+            // @ts-expect-error its tests
+            createMockEmbed(
+              MonitoringBotMessage.sdcMonitoring.failure +
+                " " +
+                `<t:${now.getTime()}:R>`,
+            ),
+          ],
+        }),
+      ),
+    ).toStrictEqual(
+      parser.handleFailure(now, guildId, userId, RemindType.SdcMonitoring),
+    );
+  });
+});
