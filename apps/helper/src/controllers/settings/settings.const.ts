11a50bc55a9a69c49be0bf29eadcb4054a52c739
ayo <a2yomidiscord@gmail.com>
2025-08-17 11:51:34 +0300

    fix: imports

---
+++ apps/helper/src/controllers/settings/settings.const.ts
@@ -0,0 +1,11 @@
+export const SettingsCustomIds = {
+  buttons: {
+    managers: {
+      roles: "settings-manage-roles",
+      channels: "settings-manage-channels",
+    },
+    updaters: {
+      panel: "settings-update-panel",
+    },
+  },
+};
---
+++ apps/helper/src/controllers/settings/settings.controller.ts
@@ -0,0 +1,24 @@
+import type { ChatInputCommandInteraction } from "discord.js";
+import { Discord, Slash } from "discordx";
+import { inject, singleton } from "tsyringe";
+
+import { HelperBotMessages } from "#/messages/index.js";
+
+import { SettingsService } from "./settings.service.js";
+
+@Discord()
+@singleton()
+export class SettingsController {
+  constructor(
+    @inject(SettingsService) private settingsService: SettingsService,
+  ) {}
+
+  @Slash({
+    name: HelperBotMessages.settings.command.name,
+    description: HelperBotMessages.settings.command.description,
+    defaultMemberPermissions: ["Administrator"],
+  })
+  handleSettings(interaction: ChatInputCommandInteraction) {
+    return this.settingsService.handleSettings(interaction);
+  }
+}
