261ff1f90c443f44fc340958266b4a2c66f1b28f
a2yomi <a2yomidiscord@gmail.com>
2025-09-18 18:52:58 +0300

    chore: backend mongo initialize

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -1,11 +1,19 @@
 package app
 
-import "github.com/gofiber/fiber/v2"
+import (
+	_ "api/cmd/docs"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
+)
 
 func Run() {
 	app := fiber.New()
 
 	// define your routes here
 
-	app.Listen(":8080")
+	api := app.Group("/api")
+
+	api.Get("/docs/*", swagger.HandlerDefault)
+
+	app.Listen(":8088")
 }
