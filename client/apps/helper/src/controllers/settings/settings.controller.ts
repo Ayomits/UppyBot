8ddfa33b1fb306b426fefd0c6befc4795a9184d2
a2yomi <a2yomidiscord@gmail.com>
2025-09-22 23:16:45 +0300

    chore: dm access

--- client/apps/helper/src/controllers/settings/settings.controller.ts
+++ client/apps/helper/src/controllers/settings/settings.controller.ts
@@ -19,6 +19,7 @@ export class SettingsController {
     name: "settings",
     description: "Настроить бота",
     defaultMemberPermissions: ["Administrator"],
+    dmPermission: false
   })
   handleSettings(interaction: ChatInputCommandInteraction) {
     return this.settingsService.handleSettingsCommand(interaction);
