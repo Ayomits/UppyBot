a3a6f6fbc75599978cfc55f0b476f1003f9a9488
a2yomi <a2yomidiscord@gmail.com>
2025-09-24 20:33:03 +0300

    refactor: remove unnecessary errors

--- client/src/errors/errors.ts
+++ client/src/errors/errors.ts
@@ -6,65 +6,6 @@ import { createError } from "./create-error.js";
 
 const ErrorTitle = "Ошибка";
 
-export const ChannelDoesNotExistsError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`Указанный канал ${bold("не существует")}`),
-  ],
-  flags: MessageFlags.Ephemeral,
-}));
-
-export const ChannelTypeError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`Указанный канал ${bold("не")} является текстовым`),
-  ],
-  flags: MessageFlags.Ephemeral,
-}));
-
-export const ModuleDisabledError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`Модуль, чьи команды Вы используете, не включен`),
-  ],
-}));
-
-export const ForbiddenError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`У Вас недостаточно прав!`),
-  ],
-  flags: MessageFlags.Ephemeral,
-}));
-
-export const SomethingWentWrongError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`Что-то пошло не так`),
-  ],
-  flags: MessageFlags.Ephemeral,
-}));
-
-export const NotHelperError = createError((interaction) => ({
-  embeds: [
-    new EmbedBuilder()
-      .setDefaults(interaction.user)
-      .setTitle(ErrorTitle)
-      .setDescription(`Указанный пользователь ${bold("не")} хелпер`),
-  ],
-  flags: MessageFlags.Ephemeral,
-}));
-
 export const UserNotFoundError = createError((interaction) => ({
   embeds: [
     new EmbedBuilder()
