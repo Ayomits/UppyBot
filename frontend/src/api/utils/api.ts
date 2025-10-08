0a3e408956d909212fe151a43eace318e3e93ac9
a2yomi <a2yomidiscord@gmail.com>
2025-10-08 15:01:39 +0300

    chore: deploy

---
+++ frontend/src/api/utils/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { Env } from "../../const/env";
+
+export const api = axios.create({
+  baseURL: Env.ApiUrl,
+  validateStatus: (status) => status >= 200 && status < 300,
+  responseType: "json",
+});
---
+++ frontend/src/const/env.ts
@@ -0,0 +1,3 @@
+export const Env = {
+  ApiUrl: import.meta.env.VITE_API_URL,
+} as const;
