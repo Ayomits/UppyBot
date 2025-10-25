c324d32acb5a89c7532c24a30f878528c43bbb3c
a2yomi <a2yomidiscord@gmail.com>
2025-10-25 18:39:30 +0300

    chore: separate settings

---
+++ bot/src/app/controllers/public/settings/v2/settings.controller.ts
@@ -0,0 +1,27 @@
+import { IsGuildUser } from "@discordx/utilities";
+import type { ChatInputCommandInteraction } from "discord.js";
+import { Discord, Guard, Slash } from "discordx";
+import { inject, singleton } from "tsyringe";
+
+import { GuildOnly } from "#/guards/is-guild-only.js";
+
+import { SettingsService } from "./settings.service.js";
+
+@Discord()
+@singleton()
+export class SettingsController {
+  constructor(
+    @inject(SettingsService) private settingsService: SettingsService
+  ) {}
+
+  @Slash({
+    name: "settings-v2",
+    description: "Настроить бота",
+    defaultMemberPermissions: ["Administrator"],
+    dmPermission: false,
+  })
+  @Guard(IsGuildUser(GuildOnly))
+  handleSettings(interaction: ChatInputCommandInteraction) {
+    return this.settingsService.handleSettingsCommand(interaction);
+  }
+}
