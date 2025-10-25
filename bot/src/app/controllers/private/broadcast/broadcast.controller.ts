19af59e20ba6c799668a23d136d7b633d682d6dd
a2yomi <a2yomidiscord@gmail.com>
2025-10-25 18:55:20 +0300

    feat: broadcast command

---
+++ bot/src/app/controllers/private/broadcast/broadcast.controller.ts
@@ -0,0 +1,44 @@
+import type {
+  MessageContextMenuCommandInteraction,
+  MessageCreateOptions,
+} from "discord.js";
+import { ApplicationCommandType } from "discord.js";
+import { ContextMenu, Discord } from "discordx";
+import { singleton } from "tsyringe";
+
+@singleton()
+@Discord()
+export class BroadcastController {
+  @ContextMenu({
+    name: "Broadcast",
+    guilds: ["1419608270959808554", "1391117548036165752"],
+    defaultMemberPermissions: ["Administrator"],
+    type: ApplicationCommandType.Message,
+  })
+  async handleBroadcast(interaction: MessageContextMenuCommandInteraction) {
+    await interaction.deferReply();
+    const client = interaction.client;
+    const msg = interaction.targetMessage;
+    const payload: MessageCreateOptions = {
+      content: msg.content,
+      embeds: msg.embeds,
+      components: msg.components,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      flags: msg.flags as any,
+    };
+    let count: number = 0;
+    await Promise.all(
+      client.guilds.cache.map(async (guild) => {
+        const owner = guild.members.cache.get(guild.ownerId);
+
+        try {
+          await owner?.send(payload);
+          count += 1;
+        } catch {
+          // nothing here
+        }
+      }),
+    );
+    interaction.editReply({ content: `Выслано: ${count} овнерам` });
+  }
+}
