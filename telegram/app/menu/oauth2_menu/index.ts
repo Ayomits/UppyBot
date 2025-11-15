6d5929580cf665ed202b78e6170f71372e21a9db
a2yomi <a2yomidiscord@gmail.com>
2025-11-15 16:06:44 +0300

    chore: add logging

--- telegram/app/menu/oauth2_menu/index.ts
+++ telegram/app/menu/oauth2_menu/index.ts
@@ -1,16 +1,24 @@
-import { Menu } from "@grammyjs/menu";
+import { Menu, MenuRange } from "@grammyjs/menu";
+import { logger } from "@typegoose/typegoose/lib/logSettings.js";
 
 import { fetchOauth2UppyUrl } from "#/shared/api/uppy/index.js";
 import { NotificationUserTokenRepository } from "#/shared/db/repositories/uppy-telegram/token.repository.js";
 import type { AppContext } from "#/telegram/utils/ctx.js";
 
-export const oauth2Menu = new Menu<AppContext>("oauth2_menu").url(
-  "Авторизироваться",
+export const oauth2Menu = new Menu<AppContext>("oauth2_menu").dynamic(
   async (ctx) => {
     const repository = NotificationUserTokenRepository.create();
     const token = await repository.sign(ctx.chatId!);
-    const payload = await fetchOauth2UppyUrl(ctx.chatId!, token);
+    const payload = await fetchOauth2UppyUrl(ctx.chatId!, token).catch(
+      logger.error
+    );
 
-    return payload?.data?.url;
-  },
+    const range = new MenuRange<AppContext>();
+
+    if (!payload) {
+      return range;
+    }
+
+    if (payload.data) return range.url(`Авторизироваться`, payload.data.url);
+  }
 );
