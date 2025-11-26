dd95c1d28e633bb2f379a59a09b0535576af78e8
a2yomi <a2yomidiscord@gmail.com>
2025-11-26 17:35:03 +0300

    fix: migration config

--- migrate.ts
+++ migrate.ts
@@ -1,7 +1,10 @@
 import { Env } from "#/shared/libs/config/index.js";
 
+import { MongoDbNames } from "./shared/db/mongo.js";
+
 export default {
   uri: Env.MongoUrl,
+  dbName: MongoDbNames.Main,
   collection: "migrations",
   migrationsPath: "./migrations",
   templatePath: "./migrations/template.ts",
